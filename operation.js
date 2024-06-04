61c4de3613dad214d3a36e2a5780786fd3530aa3
Priyanka <sarafpriyanka73737@gmail.com>
2024-06-04 19:03:17 +0530

    operation

---
+++ operation.js
@@ -0,0 +1,40 @@
+//********operations******** */
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+
+let str1 = "hello"
+let str2 = " home"
+let str3 = str1 +str2
+ console.log(str3);
+ /*string is in first all will be taken as string and if number is
+all the preceding will be taken as number"*/
+ 
+ 
+console.log("1" +2)
+console.log(1 + "2")
+console.log("1"+ 2+2);
+console.log( 1+ 2+ "2")
+console.log((2+4)*5)%4
+
+let gamecounter = 100
+++ gamecounter 
+console.log(gamecounter);
+
+let gamecounter1 = 100
+ gamecounter1 ++
+console.log(gamecounter1);
+
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
