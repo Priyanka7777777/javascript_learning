030eb6b3a2cab2cb24d248a4557b04c9106c8f0e
Priyanka <sarafpriyanka73737@gmail.com>
2024-06-04 18:47:21 +0530

    datatype conversion

---
+++ datatypeconversion.js
@@ -0,0 +1,28 @@
+let age = 33
+console.log(typeof age) // -> another way : console.log(age())
+
+/* conversion of sting to number*/
+let score = "33"
+console.log(typeof score) // -> another way : console.log(score())
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+/* "33"=> 33
+"33abc" => NAN */
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+for boolean datatype
+true =>1
+false=>0
+"" => false
+"hitest"=>true
+
+*/
+let someNumber =33
+let stringnumber = String(someNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
