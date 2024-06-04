dd0e7ff25f1273071acb7b7eb6e144864dbfb8fe
Priyanka <sarafpriyanka73737@gmail.com>
2024-06-04 17:59:03 +0530

    first commit

--- basic.js
+++ basic.js
@@ -1,2 +1,13 @@
 let a = 34;
+const f = 123
+var account = "saraf@gmail.com"
+let acc = "priya@gmail.com"
+accountcity = "bengulure"
+let city;
+
+account = "sahya@gmail.com"
+acc = "yah@gmail.com"
+ // constant cannot be chnaged
+ /* prefer not use var beacuse of issue in block and functional scope*/
 console.log(a);
+console.table([f, account,acc,accountcity,city])
