f2c0fb71ca9d8a20b6d077a45f094bb3538171c8
Priyanka <sarafpriyanka73737@gmail.com>
2024-06-04 18:14:54 +0530

    first commit

--- basic.js
+++ basic.js
@@ -11,3 +11,27 @@ acc = "yah@gmail.com"
  /* prefer not use var beacuse of issue in block and functional scope*/
 console.log(a);
 console.table([f, account,acc,accountcity,city])
+
+
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+
+// code readbility should be high
+console.log("priyanka")
+
+/* DATATYPE PRESENT -->
+number =>2 to power 53
+bigInt
+string => ""
+boolean=> true.false
+null => standalone value
+undefined => variable is created but not initialized
+    symbol => unique
+*/
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
